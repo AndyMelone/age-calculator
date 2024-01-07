ff8dd8ffdd5d31e3f1fa9373ddbadcad83fa56f6
Melone <melone@MacBook-Pro-de-Melone.local>
2024-01-07 20:26:41 +0000

    add logique

--- src/components/Period.jsx
+++ src/components/Period.jsx
@@ -3,7 +3,13 @@ export default function Period({ periodValue, period }) {
   return (
     <div className="period">
       <h1>{periodValue ? periodValue : "--"}</h1>
-      <h1>{period}</h1>
+      <h1
+        style={{
+          fontWeight: "bold",
+        }}
+      >
+        {period}
+      </h1>
     </div>
   );
 }
